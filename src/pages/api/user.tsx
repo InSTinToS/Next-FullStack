2eb261f06a1ce14845e9ccdf3a9a91a87700e073
Miguel <miguelandradebarreto2@gmail.com>
2021-06-15 13:55:28 -0300

    :rocket: ends class #2 - Create User route

--- src/pages/api/user.tsx
+++ src/pages/api/user.tsx
@@ -1,9 +1,38 @@
+import databaseConnection from 'utils/databaseConnection'
+
 import { NextApiRequest, NextApiResponse } from 'next'
 
-interface ResponseType {
-  message: string
+interface ErrorResponse {
+  error: string
 }
 
-export default (req: NextApiRequest, res: NextApiResponse<ResponseType>): void => {
-  res.status(200).json({ message: 'Hello World!' })
+interface SuccessResponse {
+  _id: string
+  name: string
+  email: string
+  teacher: string
+  cellphone: string
+}
+
+export default async (
+  req: NextApiRequest,
+  res: NextApiResponse<ErrorResponse | SuccessResponse>
+) => {
+  if (req.method === 'POST') {
+    const { name, email, cellphone, teacher } = req.body
+
+    if (!name || !email || !cellphone || !teacher)
+      return res.status(400).json({ error: 'Missing body params :(' })
+
+    const { db } = await databaseConnection()
+
+    const response = await db.collection('users').insertOne({
+      name,
+      email,
+      teacher,
+      cellphone,
+    })
+
+    res.status(200).json(response.ops[0])
+  } else res.status(400).json({ error: 'Wrong request method :(' })
 }
